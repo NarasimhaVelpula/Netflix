436e3b81c342e92adec01db9d9fbba524f7fd9fd
Narasimha <naninarasimha@gmail.com>
2020-12-09 22:35:37 +0530

    youtube version release

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import axios from './axios'
 import {useEffect,useState} from 'react';
-
+import YouTube from 'react-youtube';
 function Row({title,fetchURL, bigrow}) {
     const [movies,setMovies]=useState([])
+    const [vedioId,setVedioId]=useState([])
     const baseURL="http://image.tmdb.org/t/p/original";
+    const opts={
+        height:"390",
+        width:"100%",
+        playerVars:{
+            autoplay:1
+        }
+    }
     useEffect(() => {
        async function getMovies(){
            const response=await axios.get(fetchURL)
@@ -15,16 +23,29 @@ function Row({title,fetchURL, bigrow}) {
        getMovies();
        
     }, [fetchURL])
-    console.log(movies)
+    console.log(movies);
+    function movieHandler(movieId){
+        async function getMovieKey(){
+        const response=await axios.get("/movie/"+movieId+"/videos?api_key=7e945e91ee18f3f5b8cbb54293dc7ded&language=en-US")
+        console.log(response)
+        setVedioId(response.data.results[0].key)
+        return response;
+    }
+    getMovieKey();
+    }
     return (
         <div className="row">
             <h2 className="title" >{title}</h2>
             <div className="row_posters">
                 { movies.map((movie)=>{
-                    return(<img key={movie.id} className={bigrow ? "row_poster_big":"row_poster"} src={bigrow?(baseURL+movie.poster_path):(baseURL+movie.backdrop_path)} alt={movie.name} />)
+                    return(<img onClick={()=>{movieHandler(movie.id)}}key={movie.id} className={bigrow ? "row_poster_big":"row_poster"} src={bigrow?(baseURL+movie.poster_path):(baseURL+movie.backdrop_path)} alt={movie.name} />)
                 })}
             </div>
+            <div>
+            {vedioId!==""?<YouTube videoId={vedioId} opts={opts} />   : ""}
         </div>
+        </div>
+        
     )
 }
 
